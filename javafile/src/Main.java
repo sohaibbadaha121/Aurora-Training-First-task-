b68fd6d6e02bc38c95c25a6198f7c3b72cdf4e2b
Sohaib Badaha <sohaibbadaha@192.168.1.6>
2025-07-15 23:09:51 +0300

    ask form user to enter first number and second number

--- javafile/src/Main.java
+++ javafile/src/Main.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        Scanner scanner =new Scanner(System.in);
+
+        System.out.printf("enter the first number:");
+        double num1= scanner.nextDouble();
+
+        System.out.printf("enter the second number:");
+        double num2= scanner.nextDouble();
+
+
 
     }
 }
